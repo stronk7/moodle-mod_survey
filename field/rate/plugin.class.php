958cb97548bb887dc2510f8e3f6627e77f40fc36
kordan <kordan@mclink.it>
2013-07-24 09:02:40 +0200

    including last forgotten file into the previous commit

--- field/rate/plugin.class.php
+++ field/rate/plugin.class.php
@@ -31,7 +31,7 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot.'/mod/survey/classes/itembase.class.php');
 require_once($CFG->dirroot.'/mod/survey/field/rate/lib.php');
 
-class surveyfield_rate extends surveyitem_base {
+class surveyfield_rate extends mod_survey_itembase {
 
     /*
      * $surveyid = the id of the survey
@@ -134,12 +134,12 @@ class surveyfield_rate extends surveyitem_base {
      * @return
      */
     public function item_load($itemid) {
-        // Do parent item loading stuff here (surveyitem_base::item_load($itemid)))
+        // Do parent item loading stuff here (mod_survey_itembase::item_load($itemid)))
         parent::item_load($itemid);
 
         // multilang load support for builtin survey
         // whether executed, the 'content' field is ALWAYS handled
-        $fieldlist = array('content', 'options', 'rates', 'defaultvalue');
+        $fieldlist = $this->item_get_multilang_fields();
         $this->item_builtin_string_load_support($fieldlist);
 
         $this->item_custom_fields_to_form();
@@ -171,9 +171,10 @@ class surveyfield_rate extends surveyitem_base {
 
         // multilang save support for builtin survey
         // whether executed, the 'content' field is ALWAYS handled
-        $this->item_builtin_string_save_support($record);
+        $fieldlist = $this->item_get_multilang_fields();
+        $this->item_builtin_string_save_support($record, $fieldlist);
 
-        // Do parent item saving stuff here (surveyitem_base::item_save($record)))
+        // Do parent item saving stuff here (mod_survey_itembase::item_save($record)))
         return parent::item_save($record);
     }
 
@@ -302,6 +303,19 @@ class surveyfield_rate extends surveyitem_base {
         }
 
         return $values;
+    }
+
+    /*
+     * item_get_multilang_fields
+     *
+     * @param
+     * @return
+     */
+    public function item_get_multilang_fields() {
+        $fieldlist = parent::item_get_multilang_fields();
+        $fieldlist['rate'] = array('options', 'rates', 'defaultvalue');
+
+        return $fieldlist;
     }
 
     // MARK userform
